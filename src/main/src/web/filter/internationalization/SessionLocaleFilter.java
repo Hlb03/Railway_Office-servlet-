53075d3563e6991503d518cb06aad07249e074e9
Hlb_03 <gfietisov@gmail.com>
2022-09-08 10:05:24 +0300

    get rid of comments

--- src/main/src/web/filter/internationalization/SessionLocaleFilter.java
+++ src/main/src/web/filter/internationalization/SessionLocaleFilter.java
@@ -41,7 +41,3 @@ public class SessionLocaleFilter implements Filter {
         chain.doFilter(request, response);
     }
 }
-
-//<Connector port="8080" protocol="HTTP/1.1"
-//               connectionTimeout="20000"
-//               redirectPort="8443"/>
